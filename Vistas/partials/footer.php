188c121707e9525c162c87700e17b22c017e17e2
Maria Amparo Suarez <masuarez647@misena.edu.co>
2019-12-05 07:22:22 -0500

    PhpStorm

---
+++ Vistas/partials/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> <?= date('Y') ?>
+    </div>
+    <strong>Copyright &copy;2019 <?= date('Y') ?> <a href="<?= $baseURL; ?>">Optica-Ocular-Center</a>.</strong> Derechos Reservados.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
