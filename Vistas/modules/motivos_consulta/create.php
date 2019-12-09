b6bb2ad5af382abbe125dff8863d1fa1bd220ada
Maria Amparo Suarez <masuarez647@misena.edu.co>
2019-12-09 12:06:05 -0500

    PhpStorm

---
+++ Vistas/modules/motivos_consulta/create.php
@@ -0,0 +1,83 @@
+<?php require("../../partials/routes.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= getenv('TITLE_SITE') ?> | Motivos Consulta</title>
+    <?php require("../../partials/head_imports.php"); ?>
+</head>
+<body class="hold-transition sidebar-mini">
+
+<!-- Site wrapper -->
+<div class="wrapper">
+    <?php require("../../partials/navbar_customization.php"); ?>
+
+    <?php require("../../partials/sliderbar_main_menu.php"); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Motivos Consulta</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="<?= $baseURL; ?>/Vistas/">Optica Ocular Center</a></li>
+                            <li class="breadcrumb-item active">Inicio</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <!-- Horizontal Form -->
+            <div class="card card-info">
+                <div class="card-header">
+                    <h3 class="card-title">Motivos Consulta</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form class="form-horizontal">
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <label for="Motivos Consulta" class="col-sm-2 col-form-label">Motivos Consulta</label>
+                            <div class="col-sm-10">
+                                <input required type="text" class="form-control" id="Descripcion" name="Descripcion" placeholder="Ingrese Motivos Consulta">
+                            </div>
+                        </div>
+
+                    <div class="form-group row">
+                        <label for="Estado" class="col-sm-2 col-form-label">Estado</label>
+                        <div class="col-sm-10">
+                            <select id="Estado" name="Estado" class="custom-select">
+                                <option value="C.C">Activo</option>
+                                <option value="T.I">Inactivo</option>
+
+                            </select>
+                        </div>
+                    </div>
+
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-info">Enviar</button>
+                        <button type="submit" class="btn btn-default float-right">Cancelar</button>
+                    </div>
+                    <!-- /.card-footer -->
+                </form>
+            </div>
+            <!-- /.card -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <?php require ('../../partials/footer.php');?>
+</div>
+<!-- ./wrapper -->
+<?php require ('../../partials/scripts.php');?>
+</body>
+</html>
